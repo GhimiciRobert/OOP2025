a0a3b8dec5d6577980f1f60ad561963cc4853836
GhimiciRobert <ghimicirobert@gmail.com>
2025-03-09 15:18:40 +0200

    prob2_lab2

---
+++ lab2/prob2_update/global.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Student.h"
+
+int comparare_nume(student* student1, student* student2);
+int comparare_nota_m(student* student1, student* student2);
+int comparare_nota_e(student* student1, student* student2);
+int comparare_nota_i(student* student1, student* student2);
+int comparare_nota_finala(student* student1, student* student2);
